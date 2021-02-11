bec19fd95d755c125e52a39acf1064320e5d9c32
bradt123 <bradtbrey1234@gmail.com>
2021-02-11 11:01:50 -0400

    Modificacion controles liquidacion

--- vista/reporte_ventas/RepImpuestosVentasPropias.php
+++ vista/reporte_ventas/RepImpuestosVentasPropias.php
@@ -37,7 +37,7 @@
                           // turn on remote sorting
                           remoteSort: true,
                           //baseParams:{tipos:"''departamento'',''pais'',''localidad''",par_filtro:'nombre'}
-                          baseParams: {tipos: "''pais''", par_filtro: 'nombre'}
+                          baseParams: {tipos: "''pais''", par_filtro: 'nombre',_adicionar:'si'}
                       }),
                   valueField: 'id_lugar',
                   displayField: 'nombre',
@@ -78,13 +78,13 @@
                           totalProperty: 'total',
                           fields: ['id_catalogo', 'codigo', 'descripcion'],
                           remoteSort: true,
-                          baseParams: {cod_catalogo: 'canal_venta', par_filtro:'codigo#descripcion'}
+                          baseParams: {cod_catalogo: 'canal_venta', par_filtro:'codigo#descripcion', _adicionar:'si'}
                       }),
                   valueField: 'id_catalogo',
                   displayField: 'codigo',
                   gdisplayField: 'codigo',
                   hiddenName: 'id_catalogo',
-                  tpl:'<tpl for="."><div class="x-combo-list-item"><p style="text-transform: uppercase;"><b>{codigo}</b></p></div></tpl>',
+                  // tpl:'<tpl for="."><div class="x-combo-list-item"><p style="text-transform: uppercase;"><b>{codigo}</b></p></div></tpl>',
                   triggerAction: 'all',
                   lazyRender: true,
                   mode: 'remote',
@@ -94,9 +94,10 @@
                   width:300,
                   forceSelection: true,
                   minChars: 2,
+                  enableMultiSelect: true,
                   style:'margin-bottom: 10px;'
               },
-              type: 'ComboBox',
+              type: 'AwesomeCombo',
               filters: {pfiltro: 'lug.nombre', type: 'string'},
               id_grupo: 0,
               form: true
@@ -121,31 +122,32 @@
   	                name: 'id_punto_venta',
   	                fieldLabel: 'CODIGO IATA',
   	                allowBlank: true,
+                    disabled: true,
   	                emptyText: 'Elija la codigo iata...',
   	                store: new Ext.data.JsonStore({
   	                    url: '../../sis_ventas_facturacion/control/ReporteVentas/listarPuntoVentaRbol',
-  	                    id: 'id_punto_venta',
+  	                    id: 'codigo',
   	                    root: 'datos',
   	                    sortInfo: {
-  	                        field: 'nombre',
+  	                        field: 'codigo',
   	                        direction: 'ASC'
   	                    },
   	                    totalProperty: 'total',
-  	                    fields: ['id_punto_venta', 'nombre', 'codigo', 'office_id'],
+  	                    fields: ['codigo'],
   	                    remoteSort: true,
-  	                    baseParams: {_adicionar : 'si', offi_id:'si', par_filtro: 'codigo#nombre#office_id'}
+  	                    baseParams: {_adicionar : 'si', offi_id:'si', par_filtro: 'p.codigo'}
   	                }),
-  	                valueField: 'id_punto_venta',
+  	                valueField: 'codigo',
   	                displayField: 'codigo',
   	                gdisplayField: 'codigo',
-  	                hiddenName: 'id_punto_venta',
+  	                hiddenName: 'codigo',
   	                tpl:'<tpl for="."><div class="x-combo-list-item"><p><b>{codigo}</b></p></div></tpl>',
   	                forceSelection: true,
   	                typeAhead: false,
   	                triggerAction: 'all',
   	                lazyRender: true,
   	                mode: 'remote',
-  	                pageSize: 15,
+  	                pageSize: 25,
   	                queryDelay: 1000,
   	                gwidth: 250,
   	                width:300,
@@ -181,7 +183,7 @@
                           // turn on remote sorting
                           //baseParams:{tipos:"''departamento'',''pais'',''localidad''",par_filtro:'nombre'}
                           remoteSort: true,
-                          baseParams: {tipos: "''departamento'', ''provincia''", par_filtro: 'nombre'}
+                          baseParams: {tipos: "''departamento'', ''provincia''", par_filtro: 'nombre',_adicionar:'si'}
                       }),
                   valueField: 'id_lugar',
                   displayField: 'nombre',
@@ -205,59 +207,78 @@
               form: true
           },
           {
-      			config : {
-      				name : 'tipo',
-      				fieldLabel : 'TIPO VENTA',
-      				tinit : false,
-      				allowBlank : true,
-      				origen : 'CATALOGO',
-      				gdisplayField : 'tipo',
-              gwidth: 250,
-              width:300,
-      				valueField: 'descripcion',
-              style:'margin-bottom: 10px;',
-              tpl:'<tpl for="."><div class="x-combo-list-item"><p style="text-transform: uppercase;"><b>{codigo}</b></p></div></tpl>',
-      				baseParams : {
-      					cod_subsistema : 'VEF',
-      					catalogo_tipo : 'tipo_punto_venta',
-                // par_filtro: 'codigo'
-      				}
-      			},
-      			type : 'ComboRec',
-      			id_grupo : 1,
-      			form : true
-      		},
+              config: {
+                  name: 'tipo',
+                  fieldLabel: 'TIPO VENTA',
+                  allowBlank: true,
+                  emptyText: 'tipo venta...',
+                  store: new Ext.data.JsonStore(
+                      {
+                          url: '../../sis_ventas_facturacion/control/ReporteVentas/listarPuntoVentaTipo',
+                          id: 'tipo',
+                          root: 'datos',
+                          sortInfo: {
+                              field: 'tipo',
+                              direction: 'ASC'
+                          },
+                          totalProperty: 'total',
+                          fields: ['tipo', 'codigo'],
+                          remoteSort: true,
+                          baseParams: {_adicionar:'si'}
+                      }),
+                  valueField: 'tipo',
+                  displayField: 'tipo',
+                  gdisplayField: 'tipo',
+                  hiddenName: 'tipo',
+                  triggerAction: 'all',
+                  lazyRender: true,
+                  mode: 'remote',
+                  pageSize: 50,
+                  queryDelay: 500,
+                  gwidth: 250,
+                  width:300,
+                  forceSelection: true,
+                  minChars: 2,
+                  enableMultiSelect: true,
+                  style:'margin-bottom: 10px;'
+              },
+              type: 'AwesomeCombo',
+              filters: {pfiltro: 'lug.nombre', type: 'string'},
+              id_grupo: 1,
+              form: true
+          },
           {
   			config: {
   	                name: 'id_punto_venta_1',
   	                fieldLabel: 'OFICINA DE VENTA',
   	                allowBlank: true,
+                    disabled: true,
   	                emptyText: 'Elija la Oficina de venta...',
   	                store: new Ext.data.JsonStore({
-  	                    url: '../../sis_ventas_facturacion/control/ReporteVentas/listarPuntoVentaRbol',
+  	                    url: '../../sis_ventas_facturacion/control/ReporteVentas/listarPuntoVentaOfficeId',
                         // url: '../../sis_ventas_facturacion/control/PuntoVenta/listarPuntoVenta',
   	                    id: 'id_punto_venta',
   	                    root: 'datos',
   	                    sortInfo: {
-  	                        field: 'nombre',
+  	                        field: 'office_id',
   	                        direction: 'ASC'
   	                    },
   	                    totalProperty: 'total',
-  	                    fields: ['id_punto_venta', 'nombre', 'codigo'],
+  	                    fields: ['id_punto_venta','office_id'],
   	                    remoteSort: true,
-  	                    baseParams: {_adicionar : 'si', par_filtro:'codigo#nombre'}
+  	                    baseParams: {_adicionar : 'si', par_filtro:'office_id'}
   	                }),
   	                valueField: 'id_punto_venta',
-  	                displayField: 'nombre',
-  	                gdisplayField: 'nombre_punto_venta',
+  	                displayField: 'office_id',
+  	                gdisplayField: 'office_id',
   	                hiddenName: 'id_punto_venta',
-  	                tpl:'<tpl for="."><div class="x-combo-list-item"><p><b>Codigo:</b> {codigo}</p><p><b>Nombre:</b> {nombre}</p></div></tpl>',
+  	                tpl:'<tpl for="."><div class="x-combo-list-item"><p><b>{office_id}</b></p></div></tpl>',
   	                forceSelection: true,
   	                typeAhead: false,
   	                triggerAction: 'all',
   	                lazyRender: true,
   	                mode: 'remote',
-  	                pageSize: 15,
+  	                pageSize: 25,
   	                queryDelay: 1000,
   	                gwidth: 250,
   	                width:300,
@@ -268,7 +289,6 @@
   	            },
   	            type: 'ComboBox',
   	            id_grupo: 1,
-  	            filters: {pfiltro: 'puve.nombre',type: 'string'},
   	            form: true
   	       },
            {
@@ -276,16 +296,16 @@
                    name: 'id_moneda',
                    origen: 'MONEDA',
                    allowBlank: true,
-                   fieldLabel: 'MONEDA',
-                   gdisplayField: 'desc_moneda', //mapea al store del grid
-                   gwidth: 80,
+                   fieldLabel: 'Monenda',
+                   gdisplayField: 'desc_moneda',
+                   gwidth: 250,
                    anchor:'100%'
                },
                type: 'ComboRec',
                id_grupo: 0,
                form: true
            },
-          ],
+        ],
 
 
         title : 'Reporte de Ventas',
@@ -298,7 +318,7 @@
         constructor : function(config) {
             Phx.vista.RepImpuestosVentasPropias.superclass.constructor.call(this, config);
             this.init();
-            this.country='',this.city='',this.channel='',this.typePOS='',this.iataCode='',this.officeID='';
+            this.country='',this.city='',this.channel='',this.typePOS='',this.iataCode='',this.officeID='', this.tipo_canal='', this.code_iata='',this.moneda='';
             var fecha = new Date();
             Ext.Ajax.request({
                 url:'../../sis_parametros/control/Gestion/obtenerGestionByFecha',
@@ -319,9 +339,9 @@
 
           this.Cmp.id_lugar.store.load({params:{start:0, limit:100}, scope:this, callback: function (param,op,suc) {
 
-                  this.Cmp.id_lugar.setValue(param[5].data.id_lugar);
+                  this.Cmp.id_lugar.setValue(param[7].data.id_lugar);
                   this.Cmp.id_lugar_fk.reset();
-                  this.Cmp.id_lugar_fk.store.baseParams.id_lugar_fk = param[5].data.id_lugar;
+                  this.Cmp.id_lugar_fk.store.baseParams.id_lugar_fk = param[7].data.id_lugar;
                   this.Cmp.id_lugar_fk.modificado = true;
                   this.Cmp.id_lugar.collapse();
                   this.Cmp.id_lugar_fk.focus(false,  3);
@@ -337,22 +357,55 @@
 
           this.Cmp.id_lugar_fk.on('select',function(cmp, rec, indice){
                         this.city = rec.data.codigo.toUpperCase();
+
+                        this.Cmp.id_catalogo.reset();
+                        this.Cmp.id_catalogo.store.baseParams.id_catalogo = rec.data.codigo.toUpperCase();
+                        this.Cmp.id_catalogo.modificado = true;
           },this);
 
           this.Cmp.id_catalogo.on('select',function(cmp, rec, indice){
-                        this.channel = rec.data.codigo.toUpperCase();
+
+                        // this.channel.push(rec.data.codigo.toUpperCase());
+                        // this.tipo_canal = this.tipo_canal +','+rec.data.codigo;
+                        this.Cmp.tipo.reset();
+                        // this.Cmp.tipo.store.baseParams.tipo = this.tipo_canal.substring(1)
+                        this.Cmp.tipo.store.baseParams.tipo = this.Cmp.id_catalogo.getValue();
+                        this.Cmp.tipo.store.baseParams.id_lugar_fk = this.city;
+                        this.Cmp.tipo.modificado = true;
           },this);
 
           this.Cmp.tipo.on('select',function(cmp, rec, indice){
-                        this.typePOS = rec.data.codigo.toUpperCase();
+                        // this.typePOS = rec.data.codigo.toUpperCase();
+                        this.typePOS = this.typePOS +','+ rec.data.tipo.toUpperCase();
+                        // this.code_iata = this.code_iata +','+rec.data.tipo;
+                        this.Cmp.id_punto_venta.reset();
+                        // this.Cmp.id_punto_venta.store.baseParams.tipoVenta = this.code_iata.substring(1)
+                        this.Cmp.id_punto_venta.store.baseParams.tipoVenta = this.Cmp.tipo.getValue();
+                        this.Cmp.id_punto_venta.store.baseParams.id_lugar_fk = this.city;
+                        this.Cmp.id_punto_venta.store.baseParams.canal = this.Cmp.id_catalogo.getValue();
+                        this.Cmp.id_punto_venta.modificado = true;
+                        this.Cmp.id_punto_venta.setDisabled(false);
           },this);
 
           this.Cmp.id_punto_venta.on('select',function(cmp, rec, indice){
                         this.iataCode = rec.data.codigo.toUpperCase();
+                        this.Cmp.id_punto_venta_1.setDisabled(false);
+
+                        this.Cmp.id_punto_venta_1.reset();
+                        this.Cmp.id_punto_venta_1.store.baseParams.tipoVenta = this.Cmp.tipo.getValue();
+                        this.Cmp.id_punto_venta_1.store.baseParams.id_lugar_fk = this.city;
+                        this.Cmp.id_punto_venta_1.store.baseParams.canal = this.Cmp.id_catalogo.getValue();
+                        this.Cmp.id_punto_venta_1.store.baseParams.code_iata = this.iataCode;
+                        this.Cmp.id_punto_venta_1.modificado = true;
           },this);
 
           this.Cmp.id_punto_venta_1.on('select',function(cmp, rec, indice){
-                        this.officeID = rec.data.nombre.toUpperCase();
+                        this.officeID = rec.data.office_id.toUpperCase();
+          },this);
+
+
+          this.Cmp.id_moneda.on('select',function(cmp, rec, indice){
+                        this.moneda = rec.data.codigo_internacional.toUpperCase();
           },this);
 
         },
@@ -396,24 +449,80 @@
 
         // ActSave:'../../sis_ventas_facturacion/control/ReporteVentas/onReporteVentas',
 
+        callReport: function(resp,data){
+          var unicos = '';
+          var reg =  Ext.decode(Ext.util.Format.trim(resp.responseText));
+          unicos = reg.datos[0].codigo;
+
+          data.channel = (unicos == '')?'TODOS':unicos;
+          data.city = (data.city=='')?'TODOS':data.city;
+          // unicos = (this.channel=='')?'TODOS':unicos.substring(1);
+          data.typePOS = (data.typePOS=='')?'TODOS':data.Cmp.tipo.getValue().toUpperCase();
+          data.iataCode = (data.iataCode=='')?'TODOS':data.iataCode;
+          data.officeID = (data.officeID=='')?'TODOS':data.officeID;
+          data.moneda = (data.moneda=='')?'TODOS':data.moneda.toUpperCase();
+
+          var arg =  '/Control Ingresos/Reporte+de+Venta&rs:Command=Render&from=' + data.Cmp.fecha_ini.getValue().format('Y-m-d');
+              arg = arg + "&to=" + data.Cmp.fecha_fin.getValue().format('Y-m-d');
+              arg = arg + "&country=" + data.country;
+              arg = arg + "&city=" + data.city;
+              arg = arg + "&channel=" + data.channel;
+              arg = arg + "&typePOS=" + data.typePOS;
+              arg = arg + "&iataCode=" + data.iataCode;
+              arg = arg + "&officeID=" + data.officeID;
+              arg = arg + "&moneda=" + data.moneda;
+              arg = arg + "&rs:Format=EXCEL";
+
+              console.log("resp", arg);
+              // http://10.150.0.22:8082/Reports/Pages/Report.aspx?ItemPath=%2fBoaDwRepIngresos%2fRepImpuestosVentasPropias
+              // window.open('http://172.17.110.5:8082/BoAReportServer/Pages/RepImpuestosVentasPropias.aspx?'+arg, '_blank');
+        },
         onSubmit: function(){
         	    var me = this;
+
     			if (me.form.getForm().isValid()) {
 
             if(this.country == ''){
                 this.country = me.Cmp.id_lugar.getStore().getById(me.Cmp.id_lugar.getValue()).data.codigo.toUpperCase();
             }
-            var arg =  '/Control Ingresos/Reporte+de+Venta&rs:Command=Render&from=' + me.Cmp.fecha_ini.getValue().format('Y-m-d');
-            		arg = arg + "&to=" + me.Cmp.fecha_fin.getValue().format('Y-m-d');
-            		arg = arg + "&country=" + this.country;
-            		arg = arg + "&city=" + this.city;
-                arg = arg + "&channel=" + this.channel;
-                arg = arg + "&typePOS=" + this.typePOS;
-                arg = arg + "&iataCode=" + this.iataCode;
-                arg = arg + "&officeID=" + this.officeID;
-                arg = arg + "&rs:Format=EXCEL";
-                console.log("resp", arg);
-                // window.open('http://172.17.110.5:8082/BoAReportServer/Pages/ReportViewer.aspx?'+arg, '_blank');
+
+            if (this.Cmp.id_catalogo.getValue()!=0){
+            Ext.Ajax.request({
+                          url: '../../sis_ventas_facturacion/control/ReporteVentas/getCanal',
+                          params:{id_catalogos: this.Cmp.id_catalogo.getValue()},
+                          success: function(resp){
+                            me.callReport(resp,me)
+                          },
+                          failure: this.conexionFailure,
+                          timeout:this.timeout,
+                          scope:this
+                      });
+            }else{
+
+                this.channel='TODOS';
+                this.city = (this.city=='')?'TODOS':this.city;
+                // unicos = (this.channel=='')?'TODOS':unicos.substring(1);
+                this.typePOS = (this.typePOS=='')?'TODOS':this.Cmp.tipo.getValue().toUpperCase();
+                this.iataCode = (this.iataCode=='')?'TODOS':this.iataCode;
+                this.officeID = (this.officeID=='')?'TODOS':this.officeID;
+                this.moneda = (this.moneda=='')?'TODOS':this.moneda.toUpperCase();
+
+                var arg =  '/Control Ingresos/Reporte+de+Venta&rs:Command=Render&from=' + this.Cmp.fecha_ini.getValue().format('Y-m-d');
+                    arg = arg + "&to=" + this.Cmp.fecha_fin.getValue().format('Y-m-d');
+                    arg = arg + "&country=" + this.country;
+                    arg = arg + "&city=" + this.city;
+                    arg = arg + "&channel=" + this.channel;
+                    arg = arg + "&typePOS=" + this.typePOS;
+                    arg = arg + "&iataCode=" + this.iataCode;
+                    arg = arg + "&officeID=" + this.officeID;
+                    arg = arg + "&moneda=" + this.moneda;
+                    arg = arg + "&rs:Format=EXCEL";
+
+                    console.log("resp", arg);
+                    // http://10.150.0.22:8082/Reports/Pages/Report.aspx?ItemPath=%2fBoaDwRepIngresos%2fRepImpuestosVentasPropias
+                    // window.open('http://172.17.110.5:8082/BoAReportServer/Pages/RepImpuestosVentasPropias.aspx?'+arg, '_blank');
+            }
+
 
     			}
     		}
