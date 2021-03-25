c5c517ba68a755442aa3c95fd5028db7a3908d71
bradt123 <bradtbrey1234@gmail.com>
2021-03-25 16:20:10 -0400

    Redireccion reporte ingresos

--- vista/reporte_ventas/RepGeneralVentas.php
+++ vista/reporte_ventas/RepGeneralVentas.php
@@ -1,54 +1,10 @@
 <script>
-
-    function resizeIframe(obj) {
-        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-    }
-
-    Ext.define('Phx.vista.RepGeneralVentas',{
-        extend: 'Ext.util.Observable',
-
-        constructor: function(config) {
-            var me = this;
-            Ext.apply(this, config);
-            var me = this;
-            this.callParent(arguments);
-
-
-            this.panel = Ext.getCmp(this.idContenedor);
-            var newIndex = 3;
-
-
-
-            this.reportPanel = new Ext.Panel({
-                id: 'reportPanel-boa',
-                width: '100%',
-                height: '100%',
-                region:'center',
-                margins: '5 0 5 5',
-                layout: 'fit',
-                autoScroll : true,
-                items: [{
-                    xtype: 'box',
-                    width: '100%',
-                    height: '100%',
-                    autoEl: {
-                        tag: 'iframe',
-                        src: 'http://172.17.110.5:8082/BoAReportServer/Pages/ReportViewer.aspx?/Control%20Ingresos/Reporte+de+Venta',
-                    }}]
-            });
-
-            this.Border = new Ext.Container({
-                layout:'border',
-                id:'principal-genventa',
-                items:[this.reportPanel]
-            });
-
-            this.panel.add(this.Border);
-            this.panel.doLayout();
-            this.addEvents('init');
-            var iframe = document.getElementById('reportPanel-boa').firstChild.firstChild.firstChild;
-            iframe.style.marginTop = '-30px';
-        },
-
+  Phx.vista.RepGeneralVentas=Ext.extend(Phx.baseInterfaz,{
+        constructor:function(config) {
+          this.maestro = config.maestro;
+          Phx.vista.RepGeneralVentas.superclass.constructor.call(this, config);
+          this.panel.destroy();
+          window.open('http://xicbb-bi-dw:8082/BoAReports/browse/Control%20Ingresos', '_blank')
+          }
     });
 </script>
